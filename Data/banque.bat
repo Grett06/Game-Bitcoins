10190b19186e4e7b93764f7ffaa26c7c0f08023e
Grett06 <grett_a_bibi@hotmail.fr>
2017-06-01 14:26:39 +0200

    Update banque.bat

--- Data/banque.bat
+++ Data/banque.bat
@@ -1,11 +1,11 @@
 :START
 mode con cols=120 lines=20
 title Bitcoins ou comment devenir riche.
-rem dÈsactive l'affichage des commandes
+rem d√©sactive l'affichage des commandes
 echo off
-rem remise ‡ blanc de l'Ècran
+rem remise √† blanc de l'√©cran
 cls
-rem dÈfinition de la valeur de la variable
+rem d√©finition de la valeur de la variable
 set admin=0
 set nom=0
 set prenom=0
@@ -15,7 +15,6 @@ set chryptocodepu=0
 set chryptocodepr=0
 set chrono=0
 set day=0
-
 
 rem variable de programme
 set load=0
@@ -31,7 +30,7 @@ set saveerpargne=epargne.txt
 set savecourant=courant.txt
 
 :SAVE
-rem verification de la prÈsence du fichier si il existe alors aller vers la routine search
+rem verification de la pr√©sence du fichier si il existe alors aller vers la routine search
 if exist %savenom% goto E
 set /P nom= Quel est votre nom :
 echo %nom% > "%savenom%"
@@ -50,31 +49,30 @@ goto C
 if exist %savechryptocodepr% goto H
 set /A Chryptocodepr=%random%*%random%
 echo %chryptocodepr% > "%savechryptocodepr%"
-:I
-if exist %saveerpargne% for /f "tokens=* delims=" %%e in ('type "%saveepargne%"') do set epargne=%%e
+:D
+if exist %saveerpargne% goto I
 echo %epargne% > "%saveerpargne%"
-
-if exist %savecourant% for /f "tokens=* delims=" %%f in ('type "%savecourant%"') do set courant=%%f
-echo %courant% > "%savecourant%"
-goto LOAD
- 
-REM EXTRATION DE L INFO DANS LE FICHIER TXT ET MISE EN VARIABLE
 :E
+if exist %savecourant% goto J
+echo %courant% > "%savecourant%"
+
+REM EXTRATION DE L INFO DANS LE FICHIER TXT ET MISE EN VARIABLE
+:LOADING
 cls
 title Chargement en cours... Veuillez patienter.
 set NB_BAR=0
-set BAR=€
+set BAR=√õ
 set FULL=-1
 :UP_LOAD
 cls
 set /a FULL = FULL + 1
-set BAR=%BAR%€
+set BAR=%BAR%√õ
 set /a NB_BAR = NB_BAR + 2
 echo Recuperation des donnees .... %NB_BAR%%%
-echo ⁄ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒø
+echo √ö√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ¬ø
 echo  %BAR%
 echo  %BAR%
-echo ¿ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒŸ
+echo √Ä√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√ô
 if %FULL%==49 goto :END_LOAD
 @ping localhost -n 1 >nul
 goto :UP_LOAD
@@ -94,7 +92,16 @@ Goto C
 
 :H
 for /f "tokens=* delims=" %%d in ('type "%savechryptocodepr%"') do set chryptocodepr=%%d
-Goto I
+Goto D
+
+:I
+for /f "tokens=* delims=" %%e in ('type "%saveepargne%"') do set epargne=%%e
+Goto E
+:J
+for /f "tokens=* delims=" %%f in ('type "%savecourant%"') do set courant=%%f
+set test=1
+if /I "%test%"== (goto :PARAMETRAGE)
+goto LOAD
 
 :NP
 cls
@@ -106,36 +113,16 @@ set /P prenom= Quel est votre prenom :
 :LOAD
 title Bitcoins ou comment devenir riche.
 start Bourse.bat
-rem affiche du texte en rappelant la variable gr‚ce aux %
+rem affiche du texte en rappelant la variable gr√¢ce aux %
 echo Votre Nom est %nom%
 echo Votre Prenom est %prenom%
-
-rem annulation
-:ANNULATIONA
-set retour=0
-set /P Retour=erreur tape 1 sinon ENTREE ou Admin? :
-if /I "%retour%"=="1" (goto :NP)
-if /I "%retour%"=="0" (goto :APPLI)
-if /I "%retour%"=="admin" (goto :ADMIN)
-
-:ANNULATIONB
-set retour=0
-set /P Retour=erreur tape 1 sinon ENTREE ou Admin? :
-if /I "%retour%"=="1" (goto :NP)
-if /I "%retour%"=="0" (goto :SUITE)
-if /I "%retour%"=="admin" (goto :ADMIN)
-
-:ROUTINE
-set retour=0
-set /P Retour=Pour quitter tape 1 sinon ENTREE ? :
-if /I "%retour%"=="1" (goto :FIN)
-if /I "%retour%"=="0" (goto :SUITE)
-if /I "%retour%"=="admin" (goto :ADMIN)
+Goto JEU
 
 :ADMIN
 cls
 set /P Admin= Quel est votre le nom de L'administrateur :
 if /I "%admin%"=="%adminvalidation%" (goto :PARAMETRAGE)
+goto :ADMIN
 
 :PARAMETRAGE
 echo admin : %admin%
@@ -170,23 +157,7 @@ echo retour : %retour%
 set retour=0
 set /P retour=erreur tape 1 sinon ENTREE :
 if /I "%retour%"=="1" (goto :PARAMETRAGE)
-if /I "%retour%"=="0" (goto :SUITE)
-
-pause
-rem remise ‡ blanc de l'Ècran
-cls
-rem remise ‡ blanc
-cls
-rem affiche le texte avec la variable rentrÈe
-echo Ca va %prenom%, tu a un joli prenom :-)
-rem liste des variables
-echo Salut %USERNAME%,nous sommes le %DATE%
-echo il est %Time% dÈj‡!,
-echo %RANDOM% est un chiffre alÇatoire.
-echo Ton PC se nomme %COMPUTERNAME%,
-echo il possäde %NUMBER_OF_PROCESSORS% processeur,
-echo c'est une architecture %PROCESSOR_IDENTIFIER%
-pause
+if /I "%retour%"=="0" (goto :ROUTINE)
 
 :APPLI
 echo demarrage de l'appli banque
@@ -195,77 +166,44 @@ start appli.bat
 echo demarrage de l'appli Memo
 pause
 start memo.bat
-:SUITE
-cls
-rem Micro save
-echo %nom% > "%savenom%"
-echo %prenom% > "%saveprenom%"
-echo %chryptocodepu% > "%savechryptocodepu%"
-echo %chryptocodepr% > "%savechryptocodepr%"
-Goto JEU
 
 :JEU
 cls
 title Chargement en cours... Veuillez patienter.
 set NB_BAR=0
-set BAR=€
+set BAR=√õ
 set FULL=-1
 :UP_BAR
 cls
 set /a FULL = FULL + 1
-set BAR=%BAR%€
+set BAR=%BAR%√õ
 set /a NB_BAR = NB_BAR + 2
 echo Demarrage du PC en cours .... %NB_BAR%%%
-echo ⁄ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒø
+echo √ö√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ¬ø
 echo  %BAR%
 echo  %BAR%
-echo ¿ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒŸ
+echo √Ä√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√ô
 if %FULL%==49 goto :END_BAR
 @ping localhost -n 1 >nul
 goto :UP_BAR
 :END_BAR
 pause
 cls
+goto CHOIX
 
-:BOURSE
-title Bitcoins ou comment devenir riche.
-set retour=0
-set /P Retour=Mode Chrono 1 ou Detente 2 ? :
-if /I "%retour%"=="1" (goto :SERVEURA)
-if /I "%retour%"=="2" (goto :SERVEURB)
-if /I "%retour%"=="admin" (goto :ADMIN)
-goto ROUTINE
-
-:SERVEURA
-rem Mode Chrono
-if /I "%chrono%"==0 GOTO SERVEURAA
-set /P chrono=Combien de jours choisi ?
-cls
-:SERVEURAA
-if /I "%chrono%"==z goto SAUT
-set /a day = day + 1
-if /I "%day%"=="%chrono% GOTO FIN
-echo %chrono% jour(s) passes
-:SAUT
-set retour=0
-set /P Retour=MINAGE BITCOIN 1 ou MINAGE ETHER 2 ? :
-if /I "%retour%"=="1" (goto :BITCOIN)
-if /I "%retour%"=="2" (goto :ETHER)
+:ROUTINE
+set /P retour=erreur tape 1 sinon ENTREE :
+if /I "%retour%"=="1" (goto :PARAMETRAGE)
+if /I "%retour%"=="0" (goto :CHOIX)
 if /I "%retour%"=="admin" (goto :ADMIN)
 
-:SERVEURB
-rem Mode detente pas de jours
-cls
-set chrono=z
-goto SERVEURA
-
-:BITCOIN
-cls
-goto SERVEURA
-
-:ETHER
-cls
-goto SERVEURA
+:CHOIX
+goto SAVE
+set /P retour=Achat tape 1 Vente 2 :
+if /I "%retour%"=="0" (goto :ACHAT)
+if /I "%retour%"=="1" (goto :VENTE)
+if /I "%retour%"=="2" (goto :CHOIX)
+if /I "%retour%"=="admin" (goto :ADMIN)
 
 :FIN
 echo %chrono% jour(s) choisi
@@ -277,26 +215,34 @@ echo %nom% > "%savenom%"
 echo %prenom% > "%saveprenom%"
 echo %chryptocodepu% > "%savechryptocodepu%"
 echo %chryptocodepr% > "%savechryptocodepr%"
+echo %epargne% > "%saveepargne%"
+echo %courant% > "%savecourant%"
 TASKKILL /f /im cmd.exe
 echo fin
 goto NULL
+
+
+
+
+
+rem ressources
 
 :RESSOURCES
 cls
 title Chargement en cours... Veuillez patienter.
 set NB_BAR=0
-set BAR=€
+set BAR=√õ
 set FULL=-1
 :UP_BANQUE
 cls
 set /a FULL = FULL + 1
-set BAR=%BAR%€
+set BAR=%BAR%√õ
 set /a NB_BAR = NB_BAR + 2
-echo Connexion ‡ la banque .... %NB_BAR%%%
-echo ⁄ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒø
+echo Connexion √† la banque .... %NB_BAR%%%
+echo √ö√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ¬ø
 echo  %BAR%
 echo  %BAR%
-echo ¿ƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒƒŸ
+echo √Ä√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√Ñ√ô
 if %FULL%==49 goto :END_LOAD
 @ping localhost -n 1 >nul
 goto :UP_LOAD
@@ -306,7 +252,7 @@ goto :UP_LOAD
 rem variable pour fichier txt
 set config=config.txt
  
-rem verification de la prÈsence du fichier si il existe alors aller vers la routine search
+rem verification de la pr√©sence du fichier si il existe alors aller vers la routine search
 if exist %config% goto search
  
 set /P folder=saisir l emplacement:
@@ -331,12 +277,28 @@ for /l %%I in (1,1,50) do (
      set /a barre=%%I*2
      echo !barre!
      for /l %%A in (1,1,%%I) do (
-          set progres=!progres!€
+          set progres=!progres!√õ
      )
      echo !progres!
      ping localhost -n 1>nul
 )
 rem mode couleur
 mode con cols=65 lines=9 &color 1A
+
+pause
+rem remise √† blanc de l'√©cran
+cls
+rem remise √† blanc
+cls
+rem affiche le texte avec la variable rentr√©e
+echo Ca va %prenom%, tu a un joli prenom :-)
+rem liste des variables
+echo Salut %USERNAME%,nous sommes le %DATE%
+echo il est %Time% d√©j√†!,
+echo %RANDOM% est un chiffre al‚Äöatoire.
+echo Ton PC se nomme %COMPUTERNAME%,
+echo il poss≈†de %NUMBER_OF_PROCESSORS% processeur,
+echo c'est une architecture %PROCESSOR_IDENTIFIER%
+pause
 
 :NULL
